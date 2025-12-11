d381f10f1c38a311b7ae40b478a31f851cd2f962
Yury <not.iron.yuriy@inbox.ru>
2025-12-11 10:05:08 +0300

    feat(GraphQL): добавлен конфиг GraphQLModule (code-first, autoSchemaFile) в app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
+// src/app.module.ts
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    PrismaModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      playground: true,
+    }),
+  ],
 })
 export class AppModule {}
