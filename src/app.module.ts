e83e55bc66d174fb54589552df87be549e2742eb
Yury <not.iron.yuriy@inbox.ru>
2025-12-12 12:34:35 +0300

    feat: добавлены и подключены модули - CommentsModule, PostsModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,19 @@
-// src/app.module.ts
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { PostsModule } from 'src/graphql/posts/posts.module';
+import { CommentsModule } from './graphql/comments/comments.module';
+import { UsersModule } from './graphql/users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     PrismaModule,
+    UsersModule,
+    PostsModule,
+    CommentsModule,
+
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
